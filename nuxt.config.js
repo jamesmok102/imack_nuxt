0e640b476f66f571633e6be3a8a232e063e56e91
jamesmok102 <tpmp1093@gmail.com>
2022-07-06 16:04:50 +0800

    change title and icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'imack_nuxt',
+    title: '艾克方案有限公司 - iMACK Solutions Co., Ltd',
     htmlAttrs: {
       lang: 'en'
     },
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/imack_icon.png' }
     ]
   },
 
