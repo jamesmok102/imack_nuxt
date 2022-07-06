8fb8268e5e0e4f84d533d1ff87992e7a8900a3f6
jamesmok102 <tpmp1093@gmail.com>
2022-07-06 16:26:46 +0800

    (未啟用)增加SEO的description,og:description,og:title,og:title,og:type

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '艾克方案有限公司 - iMACK Solutions Co., Ltd',
+    title: '艾克方案有限公司 - iMACK Solutions Co., Ltd (beta)',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'zh-Hant-MO'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      //{ hid: 'description', name: 'description', content: '艾克方案有限公司成立於2013年，與天網資料科技(澳門)有限公司為同一集團旗下的子公司，主要業務為一站式先進數據中心規劃，實施交付，維護保養及弱電項目相關的安裝工程， 同時我們與業界領導者一起攜手為澳門各類客戶提供IT弱電系統、中央控制系統，大型視訊牆影顯示系統，多媒體簡報系統及公共廣播系統等方案。' },
+      //{ hid: 'og:description', name: 'og:description', content: '艾克方案有限公司成立於2013年，與天網資料科技(澳門)有限公司為同一集團旗下的子公司，主要業務為一站式先進數據中心規劃，實施交付，維護保養及弱電項目相關的安裝工程， 同時我們與業界領導者一起攜手為澳門各類客戶提供IT弱電系統、中央控制系統，大型視訊牆影顯示系統，多媒體簡報系統及公共廣播系統等方案。' },
+      //{ hid: 'og:title'  , property: 'og:title'  , content: '艾克方案有限公司 - iMACK Solutions Co., Ltd'},
+      //{ hid: 'og:type' , property: 'og:type' , content: 'website'},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/imack_icon.png' }
